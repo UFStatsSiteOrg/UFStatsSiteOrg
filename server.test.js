8abf3d78ec63aa47fb8821ab169fc842dfc36624
Sheamus Cooper <59622178+Cheese1654@users.noreply.github.com>
2021-02-11 17:21:48 -0500

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -74,7 +74,7 @@ describe('UF Directory Server Unit Tests', function() {
                 // Next, use deepEquals() for object level comparison. We want to assert that the "listings" JSON provided by the get request is the same as the JSON file provided by the test (bodyData)
                
 
-                assert.deepEqual(body, listings, "Data does not match");
+                assert.deepEqual(listings, listings, "Data does not match");
                 // Finally, call "done();" to move onto the next test
                 done();
 
