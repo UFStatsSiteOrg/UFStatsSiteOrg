960e77ce658e2fb816a09b0d5fe45b42d0d7c163
Sheamus Cooper <59622178+Cheese1654@users.noreply.github.com>
2021-02-11 13:04:36 -0500

    Update server.test.js

--- server.test.js
+++ server.test.js
@@ -36,7 +36,7 @@ describe('UF Directory Server Unit Tests', function() {
               The request module allows us to make HTTP requests. This module could also be useful in 
               making API calls to web services you make use of in your application, such as Google Maps. 
              */
-            request.get('http://localhost:8080', function(error, response, body) {
+            request.get('http://127.0.0.1:8080', function(error, response, body) {
                 /*
                   The 'should' module is an assertion library. Assertions allow us to compare the functions
                   that we are testing to the values we expect to receive back. 
@@ -64,7 +64,7 @@ describe('UF Directory Server Unit Tests', function() {
     // In these tests, we will be checking more specific content using object and primitive comparisons that have specific values.
     describe('Server provides listing data as JSON on proper request', function() {
         it('responds correctly to a GET request to "/listings"', function(done) {
-            request.get('http://localhost:8080/listings', function(error, response, body) {
+            request.get('http://127.0.0.1:8080/listings', function(error, response, body) {
 
                 // First let's assert that the body being passed by the get request actually exists or not with our general assertions, similar to the previous test:
                 assert.notEqual(body, null, "Should not be null");
@@ -83,7 +83,7 @@ describe('UF Directory Server Unit Tests', function() {
 
         // For the last test, let's use make primitive value comparisons
         it('responds with a 404 error to other GET requests', function(done) {
-            request.get('http://localhost:8080/pizza', function(error, response, body) {
+            request.get('http://127.0.0.1:8080/pizza', function(error, response, body) {
                 // First, assert that the status code is what it's supposed to be (exactly 404) if the listing were missing.
                 assert.equal(response.statusCode, 404, "404 Error");
 
