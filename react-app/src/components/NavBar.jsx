ed75d9d2d6274d360e9f990249b4e03cd01c2c55
Sheamus Cooper <sheamuscooper@protonmail.com>
2021-03-21 11:46:54 -0400

    Fix styling for NavBar

--- react-app/src/components/NavBar.jsx
+++ react-app/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default class NavBar extends Component {
 
     render () {
         return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="primary" variant={"light"}>
             <Navbar.Brand href="/">Home</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
@@ -16,20 +16,11 @@ export default class NavBar extends Component {
                     <Nav.Link href="/polls">Polls</Nav.Link>
                     <Nav.Link href="/r">Use R</Nav.Link>
                     <Nav.Link href="/internships">Internships</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+
                 </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                    <Button variant="outline-success">Search</Button>
-                </Form>
+
             </Navbar.Collapse>
         </Navbar>
-        )
+        );
     }
 }
