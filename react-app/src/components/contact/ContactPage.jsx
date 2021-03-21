d082a04431f15cd256e59c7b8e6a4ec08e6677b5
Demi Iglesias <demiiglesias@Demis-MacBook-Pro.local>
2021-03-21 14:19:01 -0400

    got backend to work, created folder, email sends

--- react-app/src/components/contact/ContactPage.jsx
+++ react-app/src/components/contact/ContactPage.jsx
@@ -1,87 +1,95 @@
-import React, { Component } from 'react';
-import axios from "axios";
+//import React, { Component } from 'react';
+import {useForm} from 'react-hook-form'; 
+import axios from 'axios';
 
-class ContactPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  
-            name: "",
-            email: "",
-            message: "",
-            status: "Submit"
+    export default function ContactPage(){
+        // eslint-disable-next-line
+        const { register, handleSubmit, errors, reset} = useForm();
+        console.log(errors);
 
+        async function onSubmitForm(values){
+            console.log(values);
+            let config = {
+            method: 'post',
+            url: `http://localhost:8080/api/contact`,
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            data:values,
         };
-    }
-    handleChange(event) {
-        const field = event.target.id;
-        if (field === "name") {
-          this.setState({ name: event.target.value });
-        } else if (field === "email") {
-          this.setState({ email: event.target.value });
-        } else if (field === "message") {
-          this.setState({ message: event.target.value });
+        try {
+            const response = await axios(config);
+            if(response.data.status === 200){
+                console.log('Success');
+            }
+            // console.log(response);
+        } catch (error) {
+            console.error(error);
         }
+        
     }
-    handleSubmit(event) {
-        event.preventDefault();  
-        this.setState({ status: "Sending" });  
-        axios({
-          method: "POST",
-          url: "http://localhost:3000/contact",
-          data: this.state,
-        }).then((response) => {
-          if (response.data.status === "sent") {
-            alert("Message Sent");
-            this.setState({ name: "", email: "", message: "", status: "Submit" });
-          } else if (response.data.status === "failed") {
-            alert("Message Failed");
-          }
-        });
-    }
-    render() { 
-        let buttonText = this.state.status;
+    
         return ( 
-        <div className="b py-16 bg-gray-50 px-4 sm:px-6 h-screen w-screen flex justify-center items-center">
+        <div className="b py-16 bg-blue-50 px-4 sm:px-6 h-screen w-screen flex justify-center items-center">
             <div className="mx-auto w-full max-w-2xl rounded-xl bg-white p-8 shadow">
                 
                 <form
-                    onSubmit={this.handleSubmit.bind(this)} 
+                    onSubmit={handleSubmit(onSubmitForm)} 
                     className="grid grid-cols-1 gap-y-6">
-                    {/* method="POST" */}
                 <div>
                     <label htmlFor="Full Name">Name:</label>
                     <input
                         type="text"
-                        id="name"
-                        value={this.state.name}
-                        onChange={this.handleChange.bind(this)}
-                        required
+                        name="name"
+                        ref={register({
+                            required:{
+                                value: true,
+                                message: 'You must enter your name',
+                            },
+                        })}
                         className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 
                         border-gray-300 rounded-md focus:outline-none focus:ring-2`}
                         placeholder="Full name"
                     />
+                    <span className="text-red-300 text -sm py-2">{errors?.name?.message}</span>
                 </div>
                 <div>
                     <label htmlFor="Email">Email:</label>
                     <input
                         type="text"
-                        id="email"
-                        value={this.state.email}
-                        onChange={this.handleChange.bind(this)}
-                        required
+                        name="email"
+                        ref={register({
+                            required:{
+                                value: true,
+                                message: 'You must enter your Email',
+                            },
+                            pattern:{
+                                value: '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$',
+                                message: 'Invalid email address',
+                            },
+                        })}
                         className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 
                         border-gray-300 rounded-md focus:outline-none focus:ring-2`}
                         placeholder="Email"
                     />
+                    <span className="text-red-300 text -sm py-2">{errors?.email?.message}</span>
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
                     <textarea 
-                    class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none inline-block align-top focus:ring-blue-500 
-                    focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2" 
                     rows="4"
-                    name="message">
+                    name="message"
+                    ref={register ({
+                        required:{
+                            value: true,
+                            message: 'Please enter a message to submit',
+                        },
+                    })}
+                    className={`w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none inline-block align-top focus:ring-blue-500 
+                    focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2`}
+                    >
                     </textarea>
+                    <span className="text-red-300 text -sm py-2">{errors?.message?.message}</span>
 
                 </div>
                 <div>
@@ -89,7 +97,7 @@ class ContactPage extends Component {
                     type="submit"
                     className="inline-flex justify-center py-3 px-9 border border-transparent shadow text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 
                     focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                        {buttonText}
+                    Submit
                     </button>
                 </div>
         </form>   
@@ -97,16 +105,3 @@ class ContactPage extends Component {
         </div>
         );
     }
-
-
-}
- 
-export default ContactPage;
-                    // {/* <textarea
-                    //     name="message"
-                    //     rows="4"
-                    //     value={this.state.message}
-                    //     onChange={this.handleChange.bind(this)}
-                    //     required
-                    //     class="block w-full shadow py-20 px-4 placeholder-gray-500 inline-block align-top focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2"
-                    //     placeholder="Message"></textarea>   */}
