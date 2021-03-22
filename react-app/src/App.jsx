e28c930b3111c2b22979af02ac8aab6f010be7bc
Sheamus Cooper <sheamuscooper@protonmail.com>
2021-03-22 16:30:50 -0400

    Import style sheet from react-bootstrap

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { HomePage, OfficerPage, InternshipPage, PollsPage, RPage, ContactPage } from './components'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
