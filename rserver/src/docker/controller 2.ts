c4fc8635d3183e5b2ba8715ad43d8fe6da1f758a
spencer <spencermehringer@gmail.com>
2021-03-28 21:58:54 -0400

    Removed error duplicate files from rserver directory

--- rserver/src/docker/controller 2.ts
+++
@@ -1,9 +0,0 @@
-import { createScriptFile } from "../io/filehandler";
-import { R } from "../util/interfaces";
-import RContainer from "./RContainer";
-
-export const runScript = async ( data : R.Data ) : Promise<string> => {
-    await createScriptFile(data);
-    let container = new RContainer(data.id);
-    return await container.runRoutine();
-}
