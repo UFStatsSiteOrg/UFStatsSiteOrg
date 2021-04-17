0ed6d7ae9a1e995592995d9b7989a2557e312614
Demi Iglesias <demiiglesias@Demis-MacBook-Pro.local>
2021-04-17 12:50:14 -0400

    add spacing to excel sheet (y axis)

--- react-app/src/components/internships/InternshipsPage.jsx
+++ react-app/src/components/internships/InternshipsPage.jsx
@@ -6,7 +6,9 @@ function InternshipsPage() {
         <div>
 
             <NavBar />
+            <div className="mt-8">
             <div align="center"> <iframe src="https://docs.google.com/spreadsheets/d/1y1xt7KoZI1-CAaIIDLihsZHNNUWpL1XXKql-vnaTnNw/edit#gid=0/pubhtml?widget=true&amp;headers=false" width="80%" height="850" ></iframe> </div>
+            </div>
         </div>
     )
 }
