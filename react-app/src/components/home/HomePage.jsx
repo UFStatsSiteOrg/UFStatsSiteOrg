805ee331814eee119ed79f7c5c614695b405191f
Demi Iglesias <demiiglesias@Demis-MacBook-Pro.local>
2021-04-17 15:03:32 -0400

    social media links

--- react-app/src/components/home/HomePage.jsx
+++ react-app/src/components/home/HomePage.jsx
@@ -1,20 +1,47 @@
+import React from "react";
 import NavBar from "../NavBar";
-import Hero from "../Hero";
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import socialMedia from "../socialMedia"
 import './HomePage.css'
 import Bulletin from "../Bulletin.jsx"
 import Img from '../logo.png'
+import ReactDom from "react-dom"
 import {Container, Row, Col} from "react-bootstrap";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import{
+    faFacebook,
+    faInstagram,
+    faLinkedin,
+    faTwitter
+
+} from '@fortawesome/free-brands-svg-icons'
+import "../socialstyle.css"
 
 function HomePage() {
     return (
         <div>
-
+            
             <NavBar>
 
             </NavBar>
-
-
+             <div className="containers">
+                <a href= "https://www.linkedin.com/company/university-of-florida-statistics-club/about/"
+                    className="linkedin social">
+                    <FontAwesomeIcon icon={faFacebook} size="2x"/>
+                </a>
+                <a href= "https://www.instagram.com/ufstatsclub/?hl=en"
+                    className="fb social">
+                    <FontAwesomeIcon icon={faInstagram} size="2x"/> 
+                </a>
+                <a href= "https://www.linkedin.com/company/university-of-florida-statistics-club/about/"
+                    className="insta social">  
+               
+                    <FontAwesomeIcon icon={faLinkedin} size="2x"/> </a>
+                <a href= "https://twitter.com/uf_stats_club?lang=en"
+                    className = "twit social">
+                    <FontAwesomeIcon icon={faTwitter}  size="2x"/>
+                    </a>
+            </div>
                 <Jumbotron class="jumbotron">
 
 
