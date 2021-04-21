715567163bbd202f8b41c08bb1e7eb8e836b0674
spencer <spencermehringer@gmail.com>
2021-04-21 13:19:07 -0400

    Change

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, '../react-app/build', 'index.html'));
 });
 
