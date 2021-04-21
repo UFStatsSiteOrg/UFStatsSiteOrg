5e22d92cfd0399f7449ff2ca7fa40ebe5f0a46c2
spencer <spencermehringer@gmail.com>
2021-04-21 12:17:24 -0400

    Build

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { HomePage, OfficerPage, InternshipPage, PollsPage, RPage, ContactPage } from './components'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
